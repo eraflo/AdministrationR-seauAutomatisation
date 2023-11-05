8d5030a6db839d3eb05c3c385b23ca5b4ed693db
fm <81640753+eraflo@users.noreply.github.com>
2023-11-05 18:39:15 +0100

    install rsat tools on remote device in the domain

--- init-project.ps1
+++ init-project.ps1
@@ -39,6 +39,36 @@ if(-not (Get-Module -ListAvailable -Name ActiveDirectory)) {
     Write-Host "Installing the module for AD powershell cmdlets..."
     Install-WindowsFeature -Name RSAT-AD-PowerShell -IncludeAllSubFeature
 }
+
+# Check if want to install the module for AD powershell cmdlets on a remote computer if there is more than one computer in the domain
+$computers = Get-ADComputer -Filter * | Select-Object -ExpandProperty Name
+if ($computers.Count -gt 1) {
+    Write-Host "Do you want to install the module for AD powershell cmdlets on a remote computer ? (y/n)"
+    
+    $answer = Read-Host
+    
+    if ($answer -eq "y") {
+        # Display the list of computers in the domain
+        Write-Host "List of computers in the domain:"
+        foreach ($computer in $computers) {
+            Write-Host $computer
+        }
+
+        # Ask the name of the computer on which we want to install the module for AD powershell cmdlets
+        Write-Host "Enter the name of the computer on which you want to install the module for AD powershell cmdlets"
+        $computerName = Read-Host
+
+        # Check if the computer exists in the domain
+        if ($computers -contains $computerName) {
+            Write-Host "Installing the module for AD powershell cmdlets on $computerName..."
+            Invoke-Command -ComputerName $computerName -ScriptBlock { Install-WindowsFeature -Name RSAT-AD-PowerShell -IncludeAllSubFeature }
+        }
+        else {
+            Write-Host "The computer $computerName does not exist in the domain"
+        }
+    }
+}
+
 # Root of the project
 $global:RootPath = "$PSScriptRoot"
 
